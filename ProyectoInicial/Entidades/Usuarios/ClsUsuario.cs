59e4864f58bb05b5b51a130785fd458abdf1103a
Andres <andres.jaag@hotmail.es>
2022-04-27 15:41:03 -0500

    Acceso de logica de negocio

---
+++ ProyectoInicial/Entidades/Usuarios/ClsUsuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+
+namespace Entidades.Usuarios
+{
+    public class ClsUsuario
+    {
+        #region Atributos privados
+        private byte _idUsuario;
+        private string _nombre, _apellido1,_apellido2;
+        private DateTime _fechaNacimiento;
+        private bool _estado;
+
+        //atributos que se conectan a la BD
+        private string _mensajeError, _valorScalar;
+        private DataTable _dtResultado;
+
+       
+        #endregion
+
+        #region Atributos publicos
+        public byte IdUsuario { get => _idUsuario; set => _idUsuario = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+       
+        public DateTime FechaNacimiento { get => _fechaNacimiento; set => _fechaNacimiento = value; }
+        public bool Estado { get => _estado; set => _estado = value; }
+        public string MensajeError { get => _mensajeError; set => _mensajeError = value; }
+        public string ValorScalar { get => _valorScalar; set => _valorScalar = value; }
+        public DataTable DtResultado { get => _dtResultado; set => _dtResultado = value; }
+        public string Apellido1 { get => _apellido1; set => _apellido1 = value; }
+        public string Apellido2 { get => _apellido2; set => _apellido2 = value; }
+        #endregion
+    }
+}
---
+++ ProyectoInicial/LogicaNegocio/Usuarios/ClsUsuarioLn.cs
@@ -0,0 +1,64 @@
+﻿using AccesoDatos.DataBase;
+using Entidades.Usuarios;
+using System;
+using System.Data;
+
+namespace LogicaNegocio.Usuarios
+{
+    public class ClsUsuarioLn
+    {
+
+        #region Variables Privadas
+        private ClsDataBase ObjDataBase = null;
+
+        #endregion
+
+        #region Metodo Index
+        public void Index(ref ClsUsuario ObjUsuario)
+        {
+            ObjDataBase = new ClsDataBase()
+            {
+                NombreTabla = "Usuarios",
+                NombreSP = "[SCH_GENERAL].[SP_Usuarios_Index]",
+                Scalar = false
+
+            };
+        }
+        #endregion
+
+        #region CRUD Usuario
+
+        #endregion
+
+        #region Metodos Privados
+        private void Ejecutar(ref ClsUsuario ObjUsuario)
+        {
+            ObjDataBase.CRUD(ref ObjDataBase);
+
+            if (ObjDataBase.MensajeErrorDB==null)
+            {
+                if (ObjDataBase.Scalar)
+                {
+                    ObjUsuario.MensajeError = ObjDataBase.MensajeErrorDB;
+                }
+                else
+                {
+                    ObjUsuario.DtResultado=ObjDataBase.DsResultados.Tables[0];
+                    if (ObjUsuario.DtResultado.Rows.Count==1)
+                    {
+                        foreach (DataRow item in ObjUsuario.DtResultado.Rows)
+                        {
+                            ObjUsuario.IdUsuario = Convert.ToByte(item["IdUsuario"].ToString());
+                            ObjUsuario.Nombre = item["Nombre"].ToString();
+                            ObjUsuario.Apellido1 = item["Apellido1"].ToString();
+                            ObjUsuario.Apellido2 = item["Apellido2"].ToString();
+                            ObjUsuario.FechaNacimiento = Convert.ToDateTime(item["FechaNacimiento"].ToString());
+                            ObjUsuario.Estado = Convert.ToBoolean(item["Estado"].ToString());
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
